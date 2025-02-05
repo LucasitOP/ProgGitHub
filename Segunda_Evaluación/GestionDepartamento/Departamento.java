1e32c9a6e7f45258c29d2ef83c6b673462d4ab6e
LucasitOP <120385407+LucasitOP@users.noreply.github.com>
2025-02-05 12:45:15 +0100

    Clase modelada

--- Segunda_Evaluación/GestionDepartamento/Departamento.java
+++ Segunda_Evaluación/GestionDepartamento/Departamento.java
@@ -1,11 +1,19 @@
 package GestionDepartamento;
 
 public class Departamento {
-String nombre;
-Profesor[] profesores;
+private String nombre;
+private Profesor[] profesores;
 
 public Departamento(String nombre,int numeroprofesores) {
 	this.nombre=nombre;
 	this.profesores=new Profesor[numeroprofesores];
+}
+public static void anyadirProfesor(Profesor[]profesores) {
+	for (int i=0;i<profesores.length;i++) {
+		if(profesores[i]==null) {
+			
+			
+		}
+	}
 }
 }
